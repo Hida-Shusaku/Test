b20a257d7dc17df5fddc978d43ed9b4bbca8f219
Hida-Shusaku <hlw81320@kwansei.ac.jp>
2023-06-14 16:58:54 +0900

    6/14

--- a3.py
+++ a3.py
@@ -6,7 +6,9 @@ with zipfile.ZipFile("sample.zip", "r") as zf:
     for i in zf.namelist():
         try:
             name = re.findall(r'_(.*?)_', i)
-            s+=int(name[0])
+            n=int(name[0])
+            if n%2!=0:
+                s+=n
         except:
             pass
 
