d99b1ededa284f03c351ec9f27ba224c48feecf3
Hida-Shusaku <hlw81320@kwansei.ac.jp>
2023-06-15 13:47:33 +0900

    6/15

--- a3.py
+++ a3.py
@@ -1,16 +1,18 @@
 import zipfile
 import re
 
-s=0
+sum=0
 with zipfile.ZipFile("sample.zip", "r") as zf:
     for i in zf.namelist():
         try:
             name = re.findall(r'_(.*?)_', i)
             n=int(name[0])
             if n%2!=0:
-                s+=n
+                with zf.open(i) as f:
+                    c = f.read()
+                    number = int(c)
+                    sum += number
         except:
             pass
 
-
-print(s)
+print("合計＝", sum)
